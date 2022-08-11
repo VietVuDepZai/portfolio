67e498bc619704b16664058e13e87a8b23f8f87a
Vietcoder <sirdang@Macintoshs-MacBook.local>
2022-08-11 15:11:05 +0700

    done

--- dennisivy/settings.py
+++ dennisivy/settings.py
@@ -52,7 +52,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
      'whitenoise.middleware.WhiteNoiseMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
